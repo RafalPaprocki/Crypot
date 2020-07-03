cb5417558a1f4589cd68ab08fd6d99bb53f27ab1
Rafal <rafalpaprocki@o2.pl>
2020-07-03 14:43:12 +0200

    User and role repositories.

---
+++ src/main/java/com/crypot/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.crypot.repository;
+
+import com.crypot.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
