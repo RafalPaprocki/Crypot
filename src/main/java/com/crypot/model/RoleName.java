f23b513b72b46f3ecb152c1a409ccd061e756a87
Rafal <rafalpaprocki@o2.pl>
2020-07-16 17:32:17 +0200

    Refactor project. New module crypot-core.

--- src/main/java/com/crypot/model/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.crypot.model;
-
-public enum  RoleName {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- src/main/java/com/crypot/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crypot.repository;
-
-import com.crypot.model.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
